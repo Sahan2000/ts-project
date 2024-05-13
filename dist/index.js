00a70139e659e851d6b29564c0acd85214a28a2f
Sahan2000 <udanasahan1@gmail.com>
2024-05-13 13:57:18 +0530

    initial the project

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function verify(emp) {
+    // verifing employee
+}
+let msg = "hello";
+exports.default = {};
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+interface Employee{
+    id: number,
+    name: string
+}
+
+function verify(emp: Employee){
+    // verifing employee
+}
+
+let msg: string = "hello";
+
+
+export default{}
